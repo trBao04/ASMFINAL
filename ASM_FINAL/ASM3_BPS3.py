37e41d03b7462e9135c03550858e4ddb3968eda2
trBao04 <163499689+trBao04@users.noreply.github.com>
2024-08-18 17:24:59 +0700

    Delete ASM_FINAL directory

--- ASM_FINAL/ASM3_BPS3.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("Dữ liệu về sản phẩm và giá.txt")
-df["ProductName"] = df["ProductName"].fillna("No Product")
-
-print(df)
-df.to_csv("Dữ liệu về sản phẩm và giá.txt", index=False)
